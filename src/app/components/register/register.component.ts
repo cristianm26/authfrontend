ab35b849df96695b69e3b6ff508311740f351add
hugo@DESKTOP-J7IENLF <cristian260893@gmail.com>
2021-09-30 21:53:31 -0500

    Auth JWT Laravel y Angular

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from 'src/app/confirmed.validator';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  form!: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) { }
+
+  createForm() {
+    this.form = this.formBuilder.group({
+      name: [null, Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: MustMatch('password', 'confirmPassword')
+    })
+  }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  submit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+  }
+
+}
